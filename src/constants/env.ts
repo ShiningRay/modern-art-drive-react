58cb2e11be8a38c4a73df181f0b7885988174a2f
ShiningRay <tsowly@hotmail.com>
2021-12-28 20:28:14 +0800

    fix building env args

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -7,16 +7,19 @@ function envArg(tag: string, defaultValue: string): string {
 export const SERVER_URL = envArg('REACT_APP_SERVER_URL', '/')
 
 // ckb
-export const CKB_NODE_URL = envArg('CKB_NODE_URL', 'https://testnet.ckb.dev')
+export const CKB_NODE_URL = envArg('REACT_APP_CKB_NODE_URL', 'https://testnet.ckb.dev')
 export const CKB_INDEXER_URL = envArg(
-  'CKB_INDEXER_URL',
+  'REACT_APP_CKB_INDEXER_URL',
   'https://testnet.ckb.dev/indexer'
 )
 export const CKB_CHAIN_ID = envArg(
-  'CKB_CHAIN_ID',
+  'REACT_APP_CKB_CHAIN_ID',
   ChainID.ckb_testnet.toString()
 )
 export const IS_MAINNET: boolean =
   CKB_CHAIN_ID !== ChainID.ckb_testnet.toString()
 // unipass
-export const UNIPASS_URL = envArg('UNIPASS_URL', 'https://t.unipass.xyz')
+export const UNIPASS_URL = envArg(
+  'REACT_APP_UNIPASS_URL',
+  'https://t.unipass.xyz'
+)
